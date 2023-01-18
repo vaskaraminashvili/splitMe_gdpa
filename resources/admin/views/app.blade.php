575fbc42f39d205611169794a6a5d1835df9bfdc
vaso <vaska.r02@gmail.com>
2023-01-18 23:18:02 +0400

    change js to scripts admin

--- resources/admin/views/app.blade.php
+++ resources/admin/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-  @vite(['resources/admin/scss/app.scss', 'resources/admin/js/app.js'])
+  @vite(['resources/admin/scss/app.scss', 'resources/admin/scripts/app.js'])
   @inertiaHead
 </head>
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from "@vitejs/plugin-vue";
-import inertia from "./resources/admin/js/vite/inertia-layout.ts";
+import inertia from "./resources/vite/inertia-layout.ts";
 
 export default defineConfig({
     plugins: [
@@ -10,7 +10,7 @@ export default defineConfig({
             include: [/\.vue$/, /\.md$/],
         }),
         laravel({
-            input: ['resources/admin/scss/app.scss', 'resources/admin/js/app.js', 'resources/web/scss/app.scss', 'resources/web/js/app.js'],
+            input: ['resources/admin/scss/app.scss', 'resources/admin/scripts/app.js', 'resources/web/scss/app.scss', 'resources/web/js/app.js'],
             refresh: true,
         }),
     ],
