88833e4d1e0fbf37305052284e8bb15ab330076c
vaska <vaska.r02@gmail.com>
2023-02-15 18:13:27 +0400

    seeders

--- database/seeders/ThreadSeeder.php
+++ database/seeders/ThreadSeeder.php
@@ -2,12 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Thread;
 use Illuminate\Database\Seeder;
 
 class ThreadSeeder extends Seeder
 {
     public function run()
     {
-
+            Thread::factory()->count(500)->create();
     }
 }
