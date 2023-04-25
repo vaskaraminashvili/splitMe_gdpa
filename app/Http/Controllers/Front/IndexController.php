9d1710357de21bcee9760c356dbe7b2df4fed06e
vaskaraminashvili <vaska.r02@gmail.com>
2023-04-25 10:22:30 +0400

    applied front slider

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers\Front;
 
+use App\Models\Slider;
 use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        $data['slides'] = [
-            '21','asd','123'
-        ];
+        $data['slides'] = Slider::query()
+            ->whereHas('media')
+            ->where('status' , 1)
+            ->orderBy('sort', 'desc')
+            ->get();
         return view("web::pages.welcome", compact('data'));
     }
 }
--- resources/web/views/pages/welcome.blade.php
+++ resources/web/views/pages/welcome.blade.php
@@ -7,43 +7,39 @@
         <ul>
           @foreach ($data['slides'] as $slide)
             <li data-transition="boxfade" data-title="Slide Title" data-param1="Additional Text"
-              data-thumb="http://localhost:8000/storage/2/conversions/purple-flowers-close-up-thumb.jpg">
+              data-thumb="{{ $slide->bigThumb() }}">
               <div class="slider-overlay"></div>
               <!-- SLIDE'S MAIN BACKGROUND IMAGE -->
-              <img src="http://localhost:8000/storage/2/conversions/purple-flowers-close-up-thumb.jpg" alt="Sky"
-                class="rev-slidebg">
+              <img src="{{ $slide->bigThumb() }}" alt="Sky" class="rev-slidebg">
               <!-- BEGIN BASIC TEXT LAYER -->
               <!-- LAYER NR. 1 -->
               <div class="tp-caption sfr font-extra-bold tp-resizeme letter-space-4 header-1 title-line-1"
-                data-x="left" data-hoffset="0" data-y="center" data-voffset="-140"
+                data-x="left" data-hoffset="0" data-y="center" data-voffset="-100"
                 data-frames='[{"delay":0,"speed":3000,"frame":"0","from":"x:[175%];y:0px;z:0;rX:0;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:1;","mask":"x:[-100%];y:0;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 6; font-size:60px; color:#fff; font-family: 'Montserrat', sans-serif;, serif; white-space: nowrap;font-weight:700;">
-                Better education for
+                style="z-index: 6; font-size:60px; color:#fff; white-space: nowrap;font-weight:700;">
+                {{ $slide->title }}
               </div>
 
-              <!-- LAYER NR. 2 -->
-              <div class="tp-caption sfr font-extra-bold tp-resizeme letter-space-4 header-1 title-line-2"
-                data-x="left" data-hoffset="0" data-y="center" data-voffset="-80"
-                data-frames='[{"delay":1000,"speed":3000,"frame":"0","from":"x:[175%];y:0px;z:0;rX:0;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:1;","mask":"x:[-100%];y:0;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 6; font-size:60px; color:#fff; font-family: 'Montserrat', sans-serif;, serif; white-space: nowrap;font-weight:700;">
-                A batter world
-              </div>
 
               <!-- LAYER NR. 3 -->
               <div class="tp-caption sfr font-extra-bold tp-resizeme letter-space-4 header-p" data-x="left"
                 data-hoffset="0" data-y="center" data-voffset="-10"
                 data-frames='[{"delay":1000,"speed":3000,"frame":"0","from":"x:[175%];y:0px;z:0;rX:0;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:1;","mask":"x:[-100%];y:0;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 6; line-height:25px; font-size:15px; color:#fff; font-family: 'Open Sans', sans-serif;, serif; white-space: nowrap;">
-                Lorem ipsum dolor sit amet, consectetuer adipiscingl elit sed diam nonumm nibhy euismod<br> tincidunt ut
-                laoreet dolore magna aliquam erat volutpat.
+                style="z-index: 6;">
+                <div class="slide_description">
+                  {!! $slide->description !!}
+                </div>
               </div>
 
               <!-- LAYER NR. 4 -->
               <div class="tp-caption lfb tp-resizeme header-btn" data-x="left" data-hoffset="0" data-y="center"
                 data-voffset="90"
                 data-frames='[{"delay":0,"speed":2000,"frame":"0","from":"y:bottom;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                style="z-index: 8;"><a href="#." class="el-btn-regular slider-btn-left">Get Started Now</a> <a
-                  href="#." class="el-btn-regular">View Courses</a>
+                style="z-index: 8;">
+                @if ($slide->link)
+                  <a href="{{ $slide->link }}" class="el-btn-regular slider-btn-left">{{ __('დეტალურად') }}</a>
+                @endif
+                {{-- <a href="#." class="el-btn-regular">View Courses</a> --}}
               </div>
             </li>
           @endforeach
